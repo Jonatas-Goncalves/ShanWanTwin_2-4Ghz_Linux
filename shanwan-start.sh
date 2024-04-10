5039bdfc2f32e3ed49a11b68af1b7622a69afb84
Jonatas Gonçalves <jonatasgoncalves@protonmail.com>
2024-04-10 01:36:56 -0300

    Fix shanwan-joystick.py path

--- shanwan-start.sh
+++ shanwan-start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 HIDRAW_DEVICE=$(dmesg | grep -oP '(?<=2563:0555\.\d{4}: input,)\w{6}\d')
-#SCRIPT_PATH=/opt/ShanWanTwin_2-4Ghz_Linux/shanwan-joystick.py
-SCRIPT_PATH=shanwan-joystick.py
+SCRIPT_PATH=/usr/share/shanwan-gamepad/shanwan-joystick.py
 
 if [ -n "$HIDRAW_DEVICE" ]
 	then
